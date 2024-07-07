8584b50b3374056625d3217b965c571ed9d1733d
dimitrionian-II <dmitry.bezlutzky@yandex.ru>
2024-07-07 18:33:41 +0500

    Normalize decimals

--- api/serializers.py
+++ api/serializers.py
@@ -1,17 +1,21 @@
+from decimal import Decimal
+
 from rest_framework import serializers
 
 from core.models import Wallet, Transaction
 
 
 class TransactionSerializer(serializers.ModelSerializer):
-    amount = serializers.DecimalField(max_digits=23, decimal_places=18, default=1000.0)
+    amount = serializers.DecimalField(
+        max_digits=23, decimal_places=18, default=1000.0, normalize_output=True
+    )
 
-    def validate_amount(self, value):
+    def validate_amount(self, value: Decimal):
         """
         Prevent a wallet balance to be negative
         """
         wallet = Wallet.objects.filter(pk=self.context.get("wallet_id")).first()
-        if wallet.balance - value < 0:
+        if wallet.balance + value < 0:
             raise serializers.ValidationError("Wallet balance cannot be negative")
 
         return value
